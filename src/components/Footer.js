83f49f75c101feb2f74fe9c68f63a85e756723ad
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-08-03 10:23:25 +0530

    Job-hiring PLatformReactJs(Assignment)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         &copy; {new Date().getFullYear()} Copyright:{' '}
         <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+          hiring Talents
         </a>
       </div>
     </MDBFooter>
